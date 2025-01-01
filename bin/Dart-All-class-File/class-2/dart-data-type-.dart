c98b0a4e52a4de50cf4e91f2d36cb8109c3aab9d
MD MOMINOR RAHMAN <mominmiu5@gmail.com>
2025-01-01 13:44:31 +0600

    class-2

---
+++ bin/Dart-All-class-File/class-2/dart-data-type-.dart
@@ -0,0 +1,13 @@
+void main(){
+  int year = 19;
+  print(year);
+  double gpa = 4.70;
+  print(gpa);
+  String city = 'my city name  is dhaka';
+  print(city);
+  bool ssc= false;
+  ssc = 4.40>4.00;
+  print ("the statement is =${ssc}");
+
+
+}
---
+++ bin/Dart-All-class-File/class-2/dart-dynamic-variable.dart
@@ -0,0 +1,15 @@
+void main () {
+  dynamic country ='london';
+  print(country);
+  dynamic name = 'mom in';
+  print (name);
+  dynamic age = '19';
+  print(age);
+  dynamic collage = 'booking';
+  print(collage);
+  dynamic  id = '317';
+  print( id);
+
+
+
+}
